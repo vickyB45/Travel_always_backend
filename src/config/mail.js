b8751233c4c73ba07bdcaf462b9b72e19a6db0d3
vimal singh <v.bisht.unic@gmail.com>
2026-01-26 11:35:25 +0530

    smtp secure true

--- src/config/mail.js
+++ src/config/mail.js
@@ -5,7 +5,7 @@ dotenv.config()
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: false, // true if 465
+  secure: true, 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
